af77029ce865023b331970d7d33e36143f901a62
Benoit Vasseur <benoit.vasseur14@gmail.com>
2013-09-12 19:43:59 +0200

    enable CORS

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,13 @@ module WebPuzzleWs
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :delete, :update]
+      end
+    end
+
   end
 end
