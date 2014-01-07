7b120ece5b0d091625eec910aaaad0488691a241
Florian Lorrain <lorrain.florian@gmail.com>
2014-01-07 11:32:32 +0100

    [FIX] Debugging authentication

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -6,24 +6,13 @@ class AuthenticationController < ApplicationController
   http_proxy '195.83.230.3', 3128 if Rails.env.development_ig2i?
   base_uri APP_CONFIG['github_api_v3']
   logger Rails.logger
-  #debug_output $stderr if Rails.env.development? || Rails.env.development_ig2i?
-  debug_output $stderr
+  debug_output $stderr if Rails.env.development? || Rails.env.development_ig2i?
 
   def github_callback
-    logger.fatal "callback github"
-    puts 'callback github'
-    puts APP_CONFIG['webpuzzle_front']
-    puts APP_CONFIG['webpuzzle_front_auth_end']
-    puts env['omniauth.auth'].inspect
-
-
     #Récupération des informations de l'utilisateur
-    #user = User.from_omniauth(env['omniauth.auth'])
+    user = User.from_omniauth(env['omniauth.auth'])
 
-    #puts user.github_token.to_s
-
-    #redirect_to APP_CONFIG['webpuzzle_front'] + APP_CONFIG['webpuzzle_front_auth_end'] + user.github_token.to_s
-    redirect_to APP_CONFIG['webpuzzle_front'] + APP_CONFIG['webpuzzle_front_auth_end'] + 'test'
+    redirect_to APP_CONFIG['webpuzzle_front'] + APP_CONFIG['webpuzzle_front_auth_end'] + user.github_token.to_s
   end
 
   def send_authentication
@@ -32,12 +21,6 @@ class AuthenticationController < ApplicationController
     #  raise Exceptions::CustomException.new I18n.t 'security.referer'
     #end
 
-    puts 'test '
-    puts Rails.env.inspect
-    puts Rails.env.production?
-    logger.fatal Rails.env.inspect
-    puts 'arrivee authentification pour le provider'
-    puts params[:provider]
     redirect_to '/auth/' + params[:provider]
   end
 
