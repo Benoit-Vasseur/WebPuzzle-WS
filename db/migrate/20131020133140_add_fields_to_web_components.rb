beb6c41fa901bae19af937954e142f2d2b07bbf1
Florian Lorrain <lorrain.florian@gmail.com>
2014-01-18 16:51:20 +0100

    [FIX] Removed unique index on githubLink

--- db/migrate/20131020133140_add_fields_to_web_components.rb
+++ db/migrate/20131020133140_add_fields_to_web_components.rb
@@ -2,6 +2,5 @@ class AddFieldsToWebComponents < ActiveRecord::Migration
   def change
     add_column :web_components, :description, :text
     add_column :web_components, :submitter, :integer
-    add_index :web_components, :githubLink, :string
   end
 end
